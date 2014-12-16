25f80ac7178a8986c096085c82ef7780195e5498
polectron <polectron@gmail.com>
2014-12-16 20:59:25 +0000

    update

--- buzzer.py
+++ buzzer.py
@@ -3,17 +3,16 @@ import RPi.GPIO as GPIO
 GPIO.setmode(GPIO.BOARD)
 GPIO.setup(12, GPIO.OUT)
 
-p = GPIO.PWM(12, 50)  # channel=12 frequency=50Hz
+p = GPIO.PWM(12, 3000)  # channel=12 frequency=3000Hz
 p.start(0)
-try:
-    while 1:
-        for dc in range(0, 101, 5):
-            p.ChangeDutyCycle(dc)
-            time.sleep(0.1)
-        for dc in range(100, -1, -5):
-            p.ChangeDutyCycle(dc)
-            time.sleep(0.1)
-except KeyboardInterrupt:
-    pass
+p.ChangeFrequency(900)
+p.ChangeDutyCycle(70)
+time.sleep(0.1)
+p.stop()
+time.sleep(0.1)
+p.start(0)
+p.ChangeFrequency(900)
+p.ChangeDutyCycle(70)
+time.sleep(0.1)
 p.stop()
 GPIO.cleanup
