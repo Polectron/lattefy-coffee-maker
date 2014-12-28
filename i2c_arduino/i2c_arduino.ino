f514d24dd1ff57539d0f14dbe6bbf7d2e3023c06
polectron <polectron@gmail.com>
2014-12-28 21:35:30 +0100

    Change melody

--- i2c_arduino/i2c_arduino.ino
+++ i2c_arduino/i2c_arduino.ino
@@ -1,5 +1,6 @@
 #include <Wire.h>
 #include <LiquidCrystal.h>
+#include "pitches.h"
 
 LiquidCrystal lcd(12, 11, 5, 4, 3, 2);
 
@@ -18,25 +19,29 @@ void setup() {
     Wire.onRequest(sendData);
 
     Serial.println("Ready!");
+    
 }
 
 void loop() {
     delay(100);
+    if (number ==  3){
+        lcd.print("Finalizado");
+        buzz();
+        number = 0;
+        lcd.clear();
+    }
 }
 
 // callback for received data
 void receiveData(int byteCount){
-
     while(Wire.available()) {
         number = Wire.read();
         lcd.setCursor(0, 0);
         lcd.clear();
-
         if (number == 1){
-            lcd.print("Haciendo opcion 1");
-        }
-        else if (number == 2){
-            lcd.print("Haciendo opcion 2");
+            lcd.print("Haciendo 1");
+        }else if (number == 2){
+            lcd.print("Haciendo 2");
         }
      }
 }
@@ -46,4 +51,27 @@ void sendData(){
     Wire.write(number);
 }
 
+void buzz(){
+    // notes in the melody:
+    int melody[] = {
+        NOTE_D3, NOTE_D4, NOTE_D5};
 
+// note durations: 4 = quarter note, 8 = eighth note, etc.:
+    int noteDurations[] = {
+        8, 8, 8};
+        
+    for (int thisNote = 0; thisNote < 8; thisNote++) {
+        // to calculate the note duration, take one second 
+        // divided by the note type.
+        //e.g. quarter note = 1000 / 4, eighth note = 1000/8, etc.
+        int noteDuration = 1000/noteDurations[thisNote];
+        tone(8, melody[thisNote],noteDuration);
+    
+        // to distinguish the notes, set a minimum time between them.
+        // the note's duration + 30% seems to work well:
+        int pauseBetweenNotes = noteDuration * 1.30;
+        delay(pauseBetweenNotes);
+        // stop the tone playing:
+        noTone(8);
+    }
+}
